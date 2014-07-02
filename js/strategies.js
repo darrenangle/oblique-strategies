bb50685c4c769f902e053f5c8fb08b82063e6303
Darren Angle <darrendarrenangle@gmail.com>
2014-07-02 14:29:32 -0500

    function and basic UI

---
+++ js/strategies.js
@@ -0,0 +1,24 @@
+(function(){
+
+	var strategize = function(){
+		var strategy = tigerMountain[Math.floor(Math.random()*tigerMountain.length)];
+		return strategy;
+	} 
+
+	$('#take').click(function(){
+		$('#strategy').html(strategize());
+	});
+
+	var tigerMountain = [
+							"Silly anxiety tapered over time and a city walk.",
+							"The dot over a lowercase 'i' is called a tittle.",
+							"Someone you don't know has a familiar face.",
+							"A generally hairy man who only drinks fizzy water. His worry and relief.",
+							"An potent Oracle who lives in a shed behind a Krispy Kreme.",
+							"Ethics and aesthetics are one. Make something unethical.",
+							"Give it room to breathe, then take it away.",
+							"A phone message, distorted accidentally, scares a woman who can't decipher it.",
+							"Several layers of fat and marbles, never mixing, hundreds of feet high.",
+							"Take Tiger Mountain by strategy." 
+						];
+})();
