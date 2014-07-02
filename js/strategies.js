e3a23e0fdb1921f1f66199556dd7b0476b2a78b5
Darren Angle <darrendarrenangle@gmail.com>
2014-07-02 14:33:05 -0500

    gh-pages

--- js/strategies.js
+++ js/strategies.js
@@ -15,7 +15,7 @@
 							"Someone you don't know has a familiar face.",
 							"A generally hairy man who only drinks fizzy water. His worry and relief.",
 							"An potent Oracle who lives in a shed behind a Krispy Kreme.",
-							"Ethics and aesthetics are one. Make something unethical.",
+							"Ethics and aesthetics are one. Make it unethical.",
 							"Give it room to breathe, then take it away.",
 							"A phone message, distorted accidentally, scares a woman who can't decipher it.",
 							"Several layers of fat and marbles, never mixing, hundreds of feet high.",
