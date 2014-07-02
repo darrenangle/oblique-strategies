d236f836dcc235bb598568d68aacd6fd4f43f7b8
Darren Angle <darrendarrenangle@gmail.com>
2014-07-02 17:05:49 -0500

    more strats and sorting

--- js/strategies.js
+++ js/strategies.js
@@ -16,25 +16,37 @@
 
 	$('.button').hide().fadeIn(1000);
 
+
 	var tigerMountain = [
-							"Silly anxiety tapered over time and a city walk",
-							"The dot over a lowercase 'i' is called a tittle",
-							"Someone you don't know has a familiar face",
-							"A generally hairy man who only drinks fizzy water - His worry and relief",
-							"A potent Oracle who lives in a shed behind a Krispy Kreme",
-							"Ethics and aesthetics are one - Make it unethical",
-							"Give it room to breathe, then take it away",
-							"A phone message, distorted accidentally, scares a woman who can't decipher it",
-							"Several layers of fat and marbles, never mixing, hundreds of feet high",
-							"Take Tiger Mountain by strategy",
-							"Its leaking and you're OK with that",
-							"Without power or intention, break their will",
-							"A paper boat pushed into a lake nearby",
-							"Loud trains on a screen in the far corner",
-							"Guitar strap or laptop bag?",
-							"Make an ocarina out of a wide carrot",
-							"A dog, turned into a rock for 40 years, remembering a bird at year 38",
-							"Your face is made of emeralds for an hour every day",
-							"Dirty bare foot cleaned in new snow" 
+							"Swing and miss", 
+							"Guitar strap or laptop bag?", 
+							"Dirty bare foot cleaned by snow", 
+							"Take Tiger Mountain by strategy", 
+							"Its leaking and you're OK with that", 
+							"Make an ocarina out of a wide carrot", 
+							"A paper boat pushed into a lake nearby", 
+							"Loud trains on a screen in the far corner", 
+							"Someone you don't know has a familiar face", 
+							"Give it room to breathe, then take it away", 
+							"Silly anxiety tapered over time and a city walk", 
+							"The dot over a lowercase 'i' is called a tittle", 
+							"Without power or intention, it breaks their will", 
+							"Ethics and aesthetics are one - Make it unethical", 
+							"Hypochondriacs meeting by a rain barrel on campus", 
+							"Your face is made of emeralds for an hour every day", 
+							"AI with hard-coded mood swings pining for a lost bell", 
+							"A potent Oracle who lives in a shed behind a Krispy Kreme", 
+							"out of the ground into the sky out of the sky into the dirt", 
+							"An android widens its stance and says, 'this is the big dream'", 
+							"A dog, turned into a rock for 40 years, remembering a bird at year 38", 
+							"Several layers of fat and marbles, never mixing, hundreds of feet high", 
+							"A generally hairy man who only drinks fizzy water - His worry and relief", 
+							"A phone message, distorted accidentally, scares a woman who can't decipher it"
+
 						];
+						
+	console.log(tigerMountain.sort(function(a, b){
+  							return a.length - b.length; 
+						}));
+
 })();
